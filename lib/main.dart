824c15d66440deea23e1fcc4da455c8a54ebe937
Vinod <binod.kumar21@outlook.com>
2021-08-25 00:29:54 +0530

    day3 commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
+import 'package:google_fonts/google_fonts.dart';
+import '/pages/login_page.dart';
 import 'package:flutter/material.dart';
-
-import 'home_page.dart';
+import 'pages/home_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,7 +11,20 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomePage(),
+      themeMode: ThemeMode.light,
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+        fontFamily: GoogleFonts.lato().fontFamily,
+      ),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+      ),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => LoginPage(),
+        '/home': (context) => HomePage(),
+        '/login': (context) => LoginPage()
+      },
     );
   }
 }
